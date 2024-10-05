d1f50d29154a6a34aa06fd5e9fb02264650b37e2
vani-madala <Vani.madala04@gmail.com>
2024-10-05 14:29:04 +0530

    Initial commit

--- prime.py
+++ prime.py
@@ -1,15 +1,4 @@
-start=int(input(""))
-end=int(input(""))
-flag=0
-if(start ==0 or start==1):
-    flag=1
-    for i in range(start,end+1):
-        flag=0
-        for j in range(2,i):
-            if(i%j==0):
-                flag=1
-        if(flag==0):
-            print(i)
+
 num=int(input())
 k=0
 for i in range(2,num+1):
