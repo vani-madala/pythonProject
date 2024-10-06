3f5a2179563de39d336e7356a91c42fab511ad26
vani-madala <Vani.madala04@gmail.com>
2024-10-06 09:45:44 +0530

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,10 @@
+numbers=int(input(""))
+for number in range(0,numbers+1):
+    if(number%3==0):
+        print("fix")
+    elif(number%5==0):
+        print("buzz")
+    elif(number%3==0 and number%5==0):
+        print("fixbuzz")
+    else:
+        print(number)
---
+++ sum of even integers.py
@@ -0,0 +1,5 @@
+number=int(input(""))
+sum=0
+for i in range(0,number+1,2):
+    sum+=i
+print(sum)
